b6b02edba363dd075adacd3492213a16b9ba3885
Ellkay\Josephe <JosephE@ellkay.com>
2018-03-05 10:55:21 -0500

    started a new examples project, for simple coding examples (e.g. scope, variable declaration, switch, etc.)

---
+++ Projects/Examples/Scope.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Examples
+{
+    public class Scope
+    {
+        // here is a private field (it's a field because it's declared in the scope of the class, and not within a function)
+        private int u = 30;
+
+        public void Example()
+        {
+            var x = 5;
+
+            int Addx(int a)
+            {
+                // this function can access the variable x because it's in the outer function scope
+                var result = a + x;
+
+                return result;
+            }
+
+            // the following statement results in an error, because the outer scope cannot access variables from the inner scope
+            //var y = result;
+
+            // however, you can access the variable x from above the function Addx - because we are still in that enclosing scope
+            // you can also access the variable u, because functions in a class have access to the class scope
+            var z = x + u;
+        }
+
+        public void OtherExample()
+        {
+            // this function has no access to any variables / functions declared in the function 'Example'
+            //Console.WriteLine(z); <-- error: The name 'z' Does not exist in the current context
+
+            Console.WriteLine(u);
+        }
+    }
+}
---
+++ Projects/Examples/Variable.cs
@@ -0,0 +1,21 @@
+﻿namespace Examples
+{
+    public class Variable
+    {
+        public void Example()
+        {
+            // both x and y (declared below) are of type integer (i.e. they are integers)
+
+            // variable declaration using var (type is inferred)
+            var x = 5;
+
+            // variable declaration specifying the type explicitly
+            int y = 5;
+
+            // some programmers prefer to specify the type explicility instead of using var - to them it is easier to read
+
+            // the downside to explicit type declarations is that when refactoring you will sometimes change the type of a variable / function result
+            // this can make refactoring take longer, as you have to change the types for all variables involved in the refactor
+        }
+    }
+}
